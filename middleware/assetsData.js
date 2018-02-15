c4780de7e90dbb386cccdb190daa2d80554ed9d6
Vladyslav Kharitonov <Vladyslav Kharitonov>
2018-02-15 22:56:26 +0200

    change assets config

--- middleware/assetsData.js
+++ middleware/assetsData.js
@@ -3,7 +3,7 @@
 module.exports = (req, res, next) => {
     const assetsData = require('../public/assets/assets.json');
     // const assetsUrl = process.env.NODE_ENV === 'production' ? '/assets/' : 'http://localhost:8050/public/assets/';
-    const assetsUrl = process.env.NODE_ENV === 'production' ? '/assets/' : 'http://localhost:8050/';
+    const assetsUrl = process.env.NODE_ENV === 'production' ? '/public/assets/' : 'http://localhost:8050/';
 
     res.locals.assetsData = {
         cssUrl: `${assetsUrl}${assetsData.main.css}`,
