416e09719f3cc6d6a50dce5ac069544a278fbdb4
Vladyslav Kharitonov <Vladyslav Kharitonov>
2018-04-22 00:49:58 +0300

    added favicon

--- src/client.js
+++ src/client.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import configureStore from './redux/store';
 import routes from './routes';
 
+const faviconUrl = '../public/favs/favicon32x32.ico';
+
 ReactDOM.render(
   <MuiThemeProvider>
+    <Favicon url={faviconUrl} />
     <Provider store={configureStore()}>
       <BrowserRouter>
         {routes}
