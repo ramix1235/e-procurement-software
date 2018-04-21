cf5066b42505cf13196964b76e40510cdab5a7d2
Vladyslav Kharitonov <Vladyslav Kharitonov>
2018-04-21 22:12:46 +0300

    small fix

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
+
 require('babel-core/register');
-['.css', '.less', '.sass', '.ttf', '.woff', '.woff2'].forEach((ext) => require.extensions[ext] = () => {});
+['.css', '.less', '.sass', '.ttf', '.woff', '.woff2'].forEach(ext => require.extensions[ext] = () => {});
 require('babel-polyfill');
 require('./app.js');
 
-// handle uncaught exceptions
 process.on('uncaughtException', (err) => {
   console.log(err.stack);
   process.exit(1);
